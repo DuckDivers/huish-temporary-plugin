4adc4115e56f742f35dde9b177948163c812de62
DuckDivers <howard@duckdiverllc.com>
2020-06-29 15:06:33 -0400

    Add Condition

--- huish-temporary-plugin.php
+++ huish-temporary-plugin.php
@@ -3,7 +3,7 @@
 * Plugin Name:       Huish Outdoors Temporary Functions
 * Plugin URI:        https://www.duckdiverllc.com
 * Description:       Temporary Functions for Huish Outdoors Sites
-* Version:           1.0.1
+* Version:           1.0.2
 * Author:            Howard Ehrenberg
 * Author URI:        https://www.howardehrenberg.com
 * License:           GPL-2.0+
@@ -14,8 +14,10 @@
 
 defined( 'ABSPATH' ) || exit;
 
-add_action('woocommerce_email_order_details', 'huish_shipping_covid_warining', 1);
-function huish_shipping_covid_warining(){
-	echo '<p><i>Due to a rapid increase in orders, combined with additional safety measures put in place due to COVID-19 concerns, we are currently experiencing longer than expected shipping times. We ask for your patience, as we recognize our delivery time this week may not meet your expectations. Our entire team is chipping in to ensure we deliver as quickly as possible.</i></p>
-	<p>Thank you for your understanding.</p>';
+add_action('woocommerce_email_order_details', 'huish_shipping_covid_warining', 1, 4);
+function huish_shipping_covid_warining( $order , $admin , $text , $email ){
+	if ($email->id == 'customer_processing_order'){
+		echo '<p><i>Due to a rapid increase in orders, combined with additional safety measures put in place due to COVID-19 concerns, we are currently experiencing longer than expected shipping times. We ask for your patience, as we recognize our delivery time this week may not meet your expectations. Our entire team is chipping in to ensure we deliver as quickly as possible.</i></p>
+		<p>Thank you for your understanding.</p>';
+	}
 }
