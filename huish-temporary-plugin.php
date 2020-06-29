9ec258caa56f9790d39e9775e2bc683419db0220
DuckDivers <howard@duckdiverllc.com>
2020-06-29 14:56:00 -0400

    Add Function

--- huish-temporary-plugin.php
+++ huish-temporary-plugin.php
@@ -11,3 +11,11 @@
 * Text Domain:       dd_theme
 * GitHub Plugin URI: https://github.com/DuckDivers/huish-temporary-plugin
 */
+
+defined( 'ABSPATH' ) || exit;
+
+add_action('woocommerce_email_order_details', 'huish_shipping_covid_warining', 1);
+function huish_shipping_covid_warining(){
+	echo '<p><i>Due to a rapid increase in orders, combined with additional safety measures put in place due to COVID-19 concerns, we are currently experiencing longer than expected shipping times. We ask for your patience, as we recognize our delivery time this week may not meet your expectations. Our entire team is chipping in to ensure we deliver as quickly as possible.</i></p>
+	<p>Thank you for your understanding.</p>';
+}
