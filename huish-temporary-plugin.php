a915f4e1d8bfe420ec92bb99fe75103009fa919d
DuckDivers <howard@duckdiverllc.com>
2020-06-29 14:52:27 -0400

    Init Plugin

---
+++ huish-temporary-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* Plugin Name:       Huish Outdoors Temporary Functions
+* Plugin URI:        https://www.duckdiverllc.com
+* Description:       Temporary Functions for Huish Outdoors Sites
+* Version:           1.0.0
+* Author:            Howard Ehrenberg
+* Author URI:        https://www.howardehrenberg.com
+* License:           GPL-2.0+
+* License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+* Text Domain:       dd_theme
+* GitHub Plugin URI: https://github.com/DuckDivers/huish-temporary-plugin
+*/
